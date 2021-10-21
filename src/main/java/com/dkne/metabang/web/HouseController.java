3253e233fc7dc48709b92eba828f057e3043d41d
0yeap95d <tjdwob@gmail.com>
2021-10-21 15:22:50 +0900

    solve cors

--- src/main/java/com/dkne/metabang/web/HouseController.java
+++ src/main/java/com/dkne/metabang/web/HouseController.java
@@ -2,6 +2,7 @@ package com.dkne.metabang.web;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.dkne.metabang.web.dto.HouseListResponseDto;
 
 import lombok.AllArgsConstructor;
 
+@CrossOrigin(origins = {"*"}, maxAge = 6000)
 @RestController
 @AllArgsConstructor
 @RequestMapping("v1")
