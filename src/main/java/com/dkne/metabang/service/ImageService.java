a13217c7168706c5165d99e01ea25235e77dcec4
qqri <cqo1154@gmail.com>
2021-10-25 15:57:45 +0900

    add all detail img , add house detail img column

--- src/main/java/com/dkne/metabang/service/ImageService.java
+++ src/main/java/com/dkne/metabang/service/ImageService.java
@@ -4,15 +4,21 @@ import com.dkne.metabang.domain.house.House;
 import com.dkne.metabang.domain.house.HouseRepository;
 import com.dkne.metabang.domain.image.Image;
 import com.dkne.metabang.domain.image.ImageRepository;
+import com.dkne.metabang.domain.review.Review;
 import com.dkne.metabang.web.dto.HouseResponseDto;
 import com.dkne.metabang.web.dto.ImageResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class ImageService {
+
+    private final HouseRepository houseRepository;
     private final ImageRepository imageRepository;
 
     @Transactional(readOnly = true)
@@ -21,6 +27,13 @@ public class ImageService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 이미지가 없습니다. id=" + image_id));
 
         return new ImageResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Image> findAll(int house_id) {
+
+        return houseRepository.findById(house_id).get().getImages();
+
     }
 
 }
